a46555bb40a790c7f2e2849f7a13793442b1170d
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-01-16 16:27:20 +0100

    add: trains table

--- database/migrations/2024_01_16_143946_create_trains_table.php
+++ database/migrations/2024_01_16_143946_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda',50);
+            $table->string('stazione_di_partenza',50);
+            $table->string('stazione_di_arrivo');
+            $table->time('orario_di_partenza', 1);
+            $table->time('orario_di_arrivo',1);
+            $table->char('codice_treno',255);
+            $table->smallInteger('numero_carrozze');
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('cancellato')->default(0);
             $table->timestamps();
         });
     }
